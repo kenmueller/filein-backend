79ffd975f47e1ae194c937ff2a08f0c96fb5f7d9
Ken Mueller <kenmueller0@gmail.com>
2020-12-01 11:46:11 -0800

    file deleted

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -7,11 +7,18 @@ admin.initializeApp({
 
 const { FieldValue } = admin.firestore
 const firestore = admin.firestore()
+const storage = admin.storage().bucket()
+
+export const fileDeleted = functions.firestore
+	.document('files/{fileId}')
+	.onDelete((_snapshot, { params: { fileId } }) =>
+		storage.file(fileId).delete()
+	)
 
 export const commentCreated = functions.firestore
 	.document('files/{fileId}/comments/{commentId}')
-	.onCreate((_snapshot, { params: { fileId } }) => {
+	.onCreate((_snapshot, { params: { fileId } }) =>
 		firestore.doc(`files/${fileId}`).update({
 			comments: FieldValue.increment(1)
 		})
-	})
+	)
