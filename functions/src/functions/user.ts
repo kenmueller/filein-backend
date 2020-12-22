d6c5db318e1956a23a71c3e9e9f9487f09dfa90a
Ken Mueller <kenmueller0@gmail.com>
2020-12-21 20:00:14 -0800

    set api key on user create

--- functions/src/functions/user.ts
+++ functions/src/functions/user.ts
@@ -1,7 +1,18 @@
 import * as functions from 'firebase-functions'
 import * as admin from 'firebase-admin'
+import { v4 as uuid } from 'uuid'
 
 const auth = admin.auth()
+
+export const userCreated = functions.firestore
+	.document('users/{uid}')
+	.onCreate(async ({ ref }) => {
+		try {
+			await ref.update({ apiKey: uuid() })
+		} catch (error) {
+			console.error(error)
+		}
+	})
 
 export const userUpdated = functions.firestore
 	.document('users/{uid}')
